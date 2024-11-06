7cdf925db1751a128394b5860ddedca31fd4e2ff
azrilrizkiansyah <azrilrizkiansyah11@gmail.com>
2024-11-06 10:27:38 +0700

    menambahkan fitur untuk menghitung keliling persegi panjang

--- main.py
+++ main.py
@@ -6,6 +6,12 @@
 panjang=float(input("masukan panjang "))
 lebar=float(input("masukan lebar"))
 
-luas= panjang * lebar
+# menghitung luas
+luas= panjang * leba
 
+# menghitung keliling 
+keliling -2 * (panjang + lebar)
+
+#menampilkan hasil luas dan keliling 
 print(f"menampilkan hasil luas persegi panjang adalah:{luas}")
+print(f"keliling persegi panjang adalah: {keliling}")
